d6ea56499fc49adc2aa701dede3d4ca2eacb67cf
NARESH-ASHOK-MALI <nareshashokmali@gmail.com>
2026-04-21 23:07:42 +0530

    cmt-21-04-2026

--- DSA/Patterns/Sudoku.java
+++ DSA/Patterns/Sudoku.java
@@ -1,27 +1,74 @@
 public class Sudoku {
     public static void main(String[] args) {
-        char board[][] = {
-            {'5','3','.','.','7','.','.','.','.'},
-            {'6','.','.','1','9','5','.','.','.'},
-            {'.','9','8','.','.','.','.','6','.'},
-            {'8','.','.','.','6','.','.','.','3'},
-            {'4','.','.','8','.','3','.','.','1'},
-            {'7','.','.','.','2','.','.','.','6'},
-            {'.','6','.','.','.','.','2','8','.'},
-            {'.','.','.','4','1','9','.','.','5'},
-            {'.','.','.','.','8','.','.','7','9'}
+        int board[][] = {
+            {5,3,0,0,7,0,0,0,0},
+            {6,0,0,1,9,5,0,0,0},
+            {0,9,8,0,0,0,0,6,0},
+            {8,0,0,0,6,0,0,0,3},
+            {4,0,0,8,0,3,0,0,1},
+            {7,0,0,0,2,0,0,0,6},
+            {0,6,0,0,0,0,2,8,0},
+            {0,0,0,4,1,9,0,0,5},
+            {0,0,0,0,8,0,0,7,9}
         };
-        Sudoku obj = new Sudoku();
-        System.out.println(obj.isValidSudoku(board));
+        if (SudokuSolver(board, 0, 0)) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    System.out.print(board[i][j] + " ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("No solution exists");
+        }
+
     }
-    public boolean isValidSudoku(char[][] board) {
+    public static boolean SudokuSolver(int board[][], int row, int col) {
+        if (row == 9) {
+            return true;
+        }
+        if (col == 9) {
+            return SudokuSolver(board, row + 1, 0);
+        }
+        if (board[row][col] != 0) {
+            return SudokuSolver(board, row, col + 1);
+        }
+
+        for (int num = 1; num <= 9; num++) {
+            if (isSafe(board, row, col, num)) {
+                board[row][col] = num;
+                if (SudokuSolver(board, row, col + 1)) {
+                    return true;
+                }
+                board[row][col] = 0; // Backtrack
+            }
+        }
+        return false;
+    }
+    public static boolean isSafe(int board[][], int row, int col, int num) {
+        // Check row
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == num) {
+                return false;
+            }
+        }
+        // Check column
         for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.' && !isValid(board, i, j)) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+        // Check 3x3 subgrid
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num) {
                     return false;
                 }
             }
         }
         return true;
     }
+
 }
