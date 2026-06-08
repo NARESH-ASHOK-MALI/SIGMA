88279fc07e9577fbde00a8e7e9514f69d78161fd
NARESH-ASHOK-MALI <nareshashokmali@gmail.com>
2026-06-08 21:56:28 +0530

    cmt-08-06-2026

---
+++ DSA/GFG/Node.java
@@ -0,0 +1,51 @@
+// Given a singly linked list, remove all nodes that have a node with a greater value anywhere to their right in the list. Return the head of the modified linked list.
+public class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+class DeleteNodesGreaterThanRight {
+    public Node removeNodes(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Reverse the linked list
+        Node prev = null;
+        Node current = head;
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        head = prev;
+
+        // Remove nodes that have a greater value to their right
+        Node maxNode = head;
+        current = head.next;
+        while (current != null) {
+            if (current.data < maxNode.data) {
+                maxNode.next = current.next; // Remove current node
+            } else {
+                maxNode = current; // Update maxNode
+            }
+            current = current.next;
+        }
+
+        // Reverse the linked list back to original order
+        prev = null;
+        current = head;
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev; // New head of the modified linked list
+    }
+}
